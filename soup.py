2c4de29ec3938167ab3688a7cd6c6f07d5930268
EdanSneh <edan.sneh@gmail.com>
2017-11-17 11:24:53 -0800

    works for machine learning

--- soup.py
+++ soup.py
@@ -1,15 +1,24 @@
 from bs4 import BeautifulSoup
 import urllib
 
-# file = open("soup.","w")
+# file = open("soup.txt","w")
 
 
-r = urllib.urlopen("http://www.dictionary.com/browse/sheep?s=t")
+r = urllib.urlopen("http://www.dictionary.com/browse/machine-learning?s=t")
 soup = BeautifulSoup(r, "html5lib")
+soup.prettify()
+
 definitions = soup.find_all("div", class_="def-set")
+
 # definitionsin = soup.find_all()
 for element in definitions:
-    sdef = element.find(class_="def-content")
-    print sdef
+    try:
+    	defcontent = element.find(class_="def-content").get_text() 
+    	defnum = element.find(class_="def-number").get_text()
+    except Exception as e:
+
+    	print e
+    print defnum + " " + defcontent.replace("  ","").replace("\n","")
 # print sdef
+
 # file.write(str(soup))
